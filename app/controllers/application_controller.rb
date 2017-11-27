5dccb374a1829187f78435fc7b0f74a56e23fdfe
Junichi Nagano <j1nagano@yahoo.co.jp>
2017-11-27 14:56:48 +0900

    deviceでログインが必須のページを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:index]
+  before_action :authenticate_customer!, if: :need_authentication?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,5 +9,15 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
       devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    end
+
+    def need_authentication?
+      not ( is_casein? or is_home_index? )
+    end
+    def is_home_index?
+      controller_name == 'home' and action_name == 'index'
+    end
+    def is_casein?
+      self.class.to_s.split('::').first == 'Casein'
     end
 end
