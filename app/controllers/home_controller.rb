1c754cfef375234ac545a52cfb6eaeca31d4a345
Junichi Nagano <j1nagano@yahoo.co.jp>
2017-11-27 23:59:41 +0900

    簡易商品リスト作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    p params
+    @items = Item.all.order(:display_order)
+  end
+
+  private
+  def home_params
+    params.require(:customer).permit(:mail, :address, :password)
   end
 end
